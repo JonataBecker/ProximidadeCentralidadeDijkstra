dacde9353c0c673eefc927d4295a3d70fddb9bed
JonataBecker <jonatabecker@gmail.com>
2015-09-09 19:49:09 -0300

    Passado a aceitar arquivo CSV por FileChooser

--- src/main/java/br/com/becker/janelas/Pricipal.java
+++ src/main/java/br/com/becker/janelas/Pricipal.java
@@ -3,16 +3,23 @@ package br.com.becker.janelas;
 import br.com.becker.centralidade.Mapa;
 import br.com.becker.centralidade.ProximidadeCentralidade;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
 
@@ -27,11 +34,29 @@ public class Pricipal extends JFrame {
         super("Proximidade centralidade");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(1000, 700);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Arquivo");
+        menu.add(new AbstractAction("Abrir") {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.showOpenDialog(null);
+                calcula(fileChooser.getSelectedFile());
+            }
+        });
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+        setLocationRelativeTo(null);
         setVisible(true);
-        calcula();
     }
 
-    private void calcula() {
+    /**
+     * Executa calculo da proximidade da centralidade
+     * 
+     * @param file Arquivo
+     */
+    private void calcula(File file) {
         try {
             Mapa mapa = new Mapa();
             mapa.loadCsv(new File("c:\\Tmp\\csv.csv"));
@@ -43,6 +68,11 @@ public class Pricipal extends JFrame {
         }
     }
 
+    /**
+     * Adiciona gráfico na tela
+     * 
+     * @param valorProximidadeCentralidade 
+     */
     private void addGrafico(List<Float> valorProximidadeCentralidade) {
         DefaultPieDataset dataset = new DefaultPieDataset();
         int nodo = 0;
@@ -58,11 +88,11 @@ public class Pricipal extends JFrame {
                 false,
                 false
         );
-
         PiePlot plot = (PiePlot) chart.getPlot();
         plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
         plot.setNoDataMessage("No data");
-        
+        PieSectionLabelGenerator generator = new StandardPieSectionLabelGenerator("{0} = {1}");
+        plot.setLabelGenerator(generator);
         final JPanel panel = new ChartPanel(chart);
         add(panel);
         SwingUtilities.invokeLater(new Runnable() {
